5d9605501cf74e29f5f6287f9ffad2695112aff5
turtle <141565698+refat-pasha@users.noreply.github.com>
2025-08-15 23:11:52 +0600

    Update todo_item.dart

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
+import '../model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
-  const ToDoItem({Key? key}) : super(key: key);
-
+  final ToDo todo;
+  final onToDoChanged;
+  final OnDeleteItem;
+  const ToDoItem({
+    Key? key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.OnDeleteItem,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {},
+        onTap: () {
+          onToDoChanged(todo);
+        },
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
-        leading: Icon(Icons.check_box, color: tdFandango),
-        title: const Text(
-          'check mail',
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdFandango,
+        ),
+        title: Text(
+          todo.todoText!,
           style: TextStyle(
             fontSize: 16,
             color: tdViolet,
-            decoration: TextDecoration.lineThrough,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
           ),
         ),
         trailing: Container(

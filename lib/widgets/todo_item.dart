a555b824351d8045cd710deddae6896bd810e997
turtle <141565698+refat-pasha@users.noreply.github.com>
2025-08-15 22:38:37 +0600

    Update todo_item.dart

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -6,34 +6,37 @@ class ToDoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {},
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      tileColor: Colors.white,
-      leading: Icon(Icons.check_box, color: tdFandango),
-      title: const Text(
-        'check mail',
-        style: TextStyle(
-          fontSize: 16,
-          color: tdViolet,
-          decoration: TextDecoration.lineThrough,
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: tdFandango),
+        title: const Text(
+          'check mail',
+          style: TextStyle(
+            fontSize: 16,
+            color: tdViolet,
+            decoration: TextDecoration.lineThrough,
+          ),
         ),
-      ),
-      trailing: Container(
-        padding: EdgeInsets.all(0),
-        margin: EdgeInsets.symmetric(vertical: 12),
-        height: 35,
-        width: 35,
-        decoration: BoxDecoration(
-          color: tdGrape,
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: IconButton(
-          onPressed: () {},
-          color: Colors.white,
-          iconSize: 18,
-          icon: Icon(Icons.delete),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdGrape,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+          ),
         ),
       ),
     );
